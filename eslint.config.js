f22c12515bd4b08ec30ff3216b9aa5e9dcd9d8d5
Jason Garber <jason@sixtwothree.org>
2023-06-29 13:29:30 -0400

    Configure space-before-function-paren

--- eslint.config.js
+++ eslint.config.js
@@ -13,8 +13,24 @@ module.exports = [
     rules: {
       ...standard.rules,
 
-      // Enforces consistent use of semicolons.
-      semi: ['error', 'always']
+      /*
+       * Enforces consistent use of semicolons.
+       *
+       * @see {@link https://eslint.org/docs/latest/rules/semi}
+       */
+      semi: ['error', 'always'],
+
+      /*
+       * Enforces consistent spacing before function parentheses and will warn
+       * whenever whitespace doesn't match the preferences specified.
+       *
+       * @see {@link https://eslint.org/docs/latest/rules/space-before-function-paren}
+       */
+      'space-before-function-paren': ['error', {
+        anonymous: 'never',
+        asyncArrow: 'always',
+        named: 'never'
+      }]
     }
   },
   {
