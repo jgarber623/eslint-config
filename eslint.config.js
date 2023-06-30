bcd91165ed2d10af0949723c9621da6e20b85108
Jason Garber <jason@sixtwothree.org>
2023-06-29 22:52:23 -0400

    Add globals package and refactor config files

--- eslint.config.js
+++ eslint.config.js
@@ -1,48 +1,15 @@
-/*
- * Import Standard's ESLint configurtation for use with ESLint's new "flat"
- * configuration file format.
- *
- * @see {@link https://github.com/standard/eslint-config-standard/blob/master/.eslintrc.json}
- * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files-new}
- */
-const standard = require('eslint-config-standard');
+const globals = require('globals');
+
+const config = require('./index');
 
 module.exports = [
+  ...config,
   {
-    plugins: Object.fromEntries(standard.plugins.map(plugin => [plugin, require(`eslint-plugin-${plugin}`)])),
-    rules: {
-      ...standard.rules,
-
-      /*
-       * Enforces consistent use of semicolons.
-       *
-       * @see {@link https://eslint.org/docs/latest/rules/semi}
-       */
-      semi: ['error', 'always'],
-
-      /*
-       * Enforces consistent spacing before function parentheses and will warn
-       * whenever whitespace doesn't match the preferences specified.
-       *
-       * @see {@link https://eslint.org/docs/latest/rules/space-before-function-paren}
-       */
-      'space-before-function-paren': ['error', {
-        anonymous: 'never',
-        asyncArrow: 'always',
-        named: 'never'
-      }]
-    }
-  },
-  {
-    /*
-     * By default, ESLint's "flat" config considers `*.js` files as ECMAScript
-     * modules (ESM). Instead, consider `*.js` files to be CommonJS modules.
-     *
-     * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files-new#configuration-objects}
-     */
-    files: ['**/*.js'],
+    files: ['spec/**/*[sS]pec.?(m)js'],
     languageOptions: {
-      sourceType: 'commonjs'
+      globals: {
+        ...globals.jasmine
+      }
     }
   }
 ];
--- index.js
+++ index.js
@@ -1,1 +1,59 @@
-module.exports = require('./eslint.config.js');
+/*
+ * Import global identifiers from different JavaScript environments.
+ *
+ * @see {@link https://www.npmjs.com/package/globals}
+ * @see {@link https://github.com/sindresorhus/globals/blob/HEAD/globals.json}
+ */
+const globals = require('globals');
+
+/*
+ * Import Standard's ESLint configurtation for use with ESLint's new "flat"
+ * configuration file format.
+ *
+ * @see {@link https://github.com/standard/eslint-config-standard/blob/master/.eslintrc.json}
+ * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files-new}
+ */
+const standard = require('eslint-config-standard');
+
+module.exports = [
+  {
+    plugins: Object.fromEntries(standard.plugins.map(plugin => [plugin, require(`eslint-plugin-${plugin}`)])),
+    rules: {
+      ...standard.rules,
+
+      /*
+       * Enforces consistent use of semicolons.
+       *
+       * @see {@link https://eslint.org/docs/latest/rules/semi}
+       */
+      semi: ['error', 'always'],
+
+      /*
+       * Enforces consistent spacing before function parentheses and will warn
+       * whenever whitespace doesn't match the preferences specified.
+       *
+       * @see {@link https://eslint.org/docs/latest/rules/space-before-function-paren}
+       */
+      'space-before-function-paren': ['error', {
+        anonymous: 'never',
+        asyncArrow: 'always',
+        named: 'never'
+      }]
+    }
+  },
+  {
+    /*
+     * By default, ESLint's "flat" config considers `*.js` files as ECMAScript
+     * modules (ESM). Instead, consider `*.js` files to be CommonJS modules.
+     *
+     * @see {@link https://eslint.org/docs/latest/use/configure/configuration-files-new#configuration-objects}
+     */
+    files: ['**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.commonjs
+      },
+      sourceType: 'commonjs'
+    }
+  }
+];
