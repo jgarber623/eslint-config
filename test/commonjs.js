42ffb7da291ffe8285d024b1acaa689038b56f45
Jason Garber <jason@sixtwothree.org>
2023-09-29 23:24:28 -0400

    Add test and coverage collection

---
+++ test/commonjs.js
@@ -0,0 +1,39 @@
+const test = require('ava');
+const { FlatESLint } = require('eslint/use-at-your-own-risk');
+
+const config = require('../commonjs');
+
+let eslint;
+
+test.before(() => {
+  eslint = new FlatESLint({ baseConfig: config });
+});
+
+test('exports an array', t => {
+  t.true(Array.isArray(config));
+});
+
+test('loads config and validates correct syntax', async (t) => {
+  const [results] = await eslint.lintText('module.exports = [];\n');
+
+  t.is(results.errorCount, 0);
+});
+
+test('loads config and invalidates incorrect syntax', async (t) => {
+  const [results] = await eslint.lintText('export default [];\n');
+
+  const expected = [
+    {
+      column: 1,
+      fatal: true,
+      line: 1,
+      message: 'Parsing error: \'import\' and \'export\' may appear only with \'sourceType: module\'',
+      nodeType: null,
+      ruleId: null,
+      severity: 2,
+    }
+  ];
+
+  t.is(results.errorCount, 1);
+  t.deepEqual(results.messages, expected);
+});
