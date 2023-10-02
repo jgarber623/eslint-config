833cc144e12c8e77b5097f01eef2fa5a34ca851d
Jason Garber <jason@sixtwothree.org>
2023-10-02 14:38:41 -0400

    Remove remaining Jasmine-related config

--- eslint.config.js
+++ eslint.config.js
@@ -1,15 +1,1 @@
-const globals = require('globals');
-
-const config = require('./lib/index');
-
-module.exports = [
-  ...config,
-  {
-    files: ['spec/**/*[sS]pec.?(m)js'],
-    languageOptions: {
-      globals: {
-        ...globals.jasmine
-      }
-    }
-  }
-];
+module.exports = require('./lib/index');
