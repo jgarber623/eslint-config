3502bfabf1d93c49cc4d48ca529aa1cfa38fb1f2
Jason Garber <jason@sixtwothree.org>
2023-10-03 22:51:21 -0400

    Require ava config in project config

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,6 @@
  */
 module.exports = [
   ...(require('@jgarber/eslint-config/commonjs')),
-  ...(require('@jgarber/eslint-config'))
+  ...(require('@jgarber/eslint-config')),
+  ...(require('@jgarber/eslint-config/ava'))
 ];
