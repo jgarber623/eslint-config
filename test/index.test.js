c815bb3a56b427af6644edd5d574a996f2979102
Jason Garber <jason@sixtwothree.org>
2023-10-04 09:53:27 -0400

    Configure eslint-plugin-array-func

--- test/index.test.js
+++ test/index.test.js
@@ -14,35 +14,47 @@ test('loads config and validates correct syntax', async (t) => {
 });
 
 test('loads config and invalidates incorrect syntax', async (t) => {
-  const [{ errorCount, messages }] = await eslint.lintText('(() => {}) ()\n');
+  const [{ errorCount, messages }] = await eslint.lintText('(() => { [].map().flat(); }) ()\n');
 
   const expected = [
+    {
+      ruleId: 'array-func/prefer-flat-map',
+      severity: 2,
+      message: 'Use flatMap instead of .map().flat()',
+      line: 1,
+      column: 13,
+      nodeType: null,
+      messageId: 'preferFlatMap',
+      endLine: 1,
+      endColumn: 25,
+      fix: { range: [12, 24], text: 'flatMap()' }
+    },
     {
       ruleId: 'func-call-spacing',
       severity: 2,
       message: 'Unexpected whitespace between function name and paren.',
       line: 1,
-      column: 11,
+      column: 29,
       nodeType: 'CallExpression',
       messageId: 'unexpectedWhitespace',
       endLine: 1,
-      endColumn: 11,
-      fix: { range: [10, 11], text: '' }
+      endColumn: 29,
+      fix: { range: [28, 29], text: '' }
     },
     {
       ruleId: 'semi',
       severity: 2,
       message: 'Missing semicolon.',
       line: 1,
-      column: 14,
+      column: 32,
       nodeType: 'ExpressionStatement',
       messageId: 'missingSemi',
       endLine: 2,
       endColumn: 1,
-      fix: { range: [13, 13], text: ';' }
+      fix: { range: [31, 31], text: ';' }
     }
   ];
 
-  t.is(errorCount, 2);
+  t.is(errorCount, 3);
   t.deepEqual(messages, expected);
 });
