034b9cc807730169aca574882351102f8e0d33c2
Jason Garber <jason@sixtwothree.org>
2023-10-15 20:57:36 -0400

    Minor tweaks (#34)
    * Move engines config to the bottom
    
    * Use double-slash comment style

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,7 @@
-/**
- * Require files using the package's full name and configured exports.
- *
- * @see {@link https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#package-entry-points}
- * @see {@link https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#self-referencing-a-package-using-its-name}
- */
+// Require files using the package's full name and configured exports.
+//
+// https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#package-entry-points
+// https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#self-referencing-a-package-using-its-name
 module.exports = [
   {
     ignores: ['coverage']
