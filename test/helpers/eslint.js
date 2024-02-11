7c0a5a0d2dfd0e08b28178150ac567f0cc9bf949
Jason Garber <jason@sixtwothree.org>
2024-02-11 16:44:06 -0500

    Remove old test files

--- test/helpers/eslint.js
+++
@@ -1,8 +0,0 @@
-const { FlatESLint } = require('eslint/use-at-your-own-risk');
-
-module.exports = function(baseConfig) {
-  return new FlatESLint({
-    baseConfig,
-    overrideConfigFile: true
-  });
-};
