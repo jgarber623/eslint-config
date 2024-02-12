55a9aa84efdc0967e7cc82a51d1928caf2c22937
Jason Garber <jason@sixtwothree.org>
2024-02-11 23:31:41 -0500

    Update project ESLint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,20 @@
-// Require files using the package's full name and configured exports.
-//
-// https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#package-entry-points
-// https://nodejs.org/dist/latest-v18.x/docs/api/packages.html#self-referencing-a-package-using-its-name
-module.exports = [
+/**
+ * Import package using its full name and configured default export.
+ *
+ * @see {@link https://nodejs.org/dist/latest-v20.x/docs/api/packages.html#package-entry-points}
+ * @see {@link https://nodejs.org/dist/latest-v20.x/docs/api/packages.html#self-referencing-a-package-using-its-name}
+ */
+import eslintConfig from "@jgarber/eslint-config";
+
+export default [
+  ...eslintConfig,
   {
-    ignores: ['coverage']
+    files: ["**/*.cjs"],
+    languageOptions: {
+      globals: {
+        module: "readonly",
+        require: "readonly",
+      },
+    },
   },
-  ...(require('@jgarber/eslint-config/commonjs')),
-  ...(require('@jgarber/eslint-config')),
-  ...(require('@jgarber/eslint-config/ava'))
 ];
